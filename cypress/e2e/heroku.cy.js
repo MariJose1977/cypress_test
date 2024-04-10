545d8e78343df462131136e1099abd94378b5506
Maria Jose <marijose1977@gmail.com>
2024-04-10 20:25:52 +0200

    Finalizadoo

--- cypress/e2e/heroku.cy.js
+++ cypress/e2e/heroku.cy.js
@@ -1,10 +1,23 @@
 describe('template spec', () => {
-it ('test de login valido', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
+    it('test de login valido', () => {
+    cy.visit('https://the-internet.herokuapp.com')
     cy.get(':nth-child(21) > a').click()
-    cy.get('#username').type("tomsmith")
+    cy.get('#username').type('tomsmith')
     cy.get('#password').type('SuperSecretPassword!')
-    cy.get('.fa').click()
+    cy.get('.radius').click()
     cy.get('#flash').contains('You logged into a secure area!')
- })
+  })
+  it('test de login invalido', () => {
+    cy.visit('https://the-internet.herokuapp.com')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmit')
 })
+it('test de login invalido', () => {
+    cy.visit('https://the-internet.herokuapp.com')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword')
+})
+
+})
+
